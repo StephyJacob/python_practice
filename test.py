af55653aac988d28a488b87b6af6c1b3085cc62e
Stephy Jacob <mrstephy.jacob@gmail.com>
2020-04-10 16:07:10 +0530

    greet changes

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-import sys
+def greet(who_to_greet):
+    greeting = "Hello, " + who_to_greet
+    return greeting
 
-print (sys.version)
-print (sys.executable)
+if __name__ == "__main__":
+    nameToGreet = input("Enter the name to be greeted: ")
+    print (greet(nameToGreet))
